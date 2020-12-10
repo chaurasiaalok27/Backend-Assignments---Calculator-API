209c2c8b662ac64a784db8fe22c1c4b2c268feb0
Alok Chaurasia <chaurasiaalok4@gmail.com>
2020-12-10 17:16:26 +0530

    sub

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ app.get('',(req,res)=>
 {
     res.send("Hello World!");
 });
+
+
 app.post('/add',(req,res)=>
 {
     const obj1={
@@ -53,7 +55,42 @@ app.post('/add',(req,res)=>
     res.send(obj1);
 });
 
+app.post('/sub',(req,res)=>
+{
+    const obj1={
+        status: "",
+        message: "",
+        sum: 0,
+    };
+    const num1 = Number(req.body.num1);
+    const num2 = Number(req.body.num2);
+    
 
+    if(isNaN(num1) || isNaN(num2))
+    {
+        obj1.status="error";
+        obj1.message="Invalid data type";
+    }
+    else{
+        const result = num1-num2;
+        if(num1 < -1000000 ||num2 < -1000000|| result < -1000000)
+        {
+            obj1.status="error";
+        obj1.message="Underflow";
+        } else if(num1 > 1000000 ||num2 > 1000000|| result > 1000000)
+        {
+            obj1.status="error";
+            obj1.message="Overflow";
+        } else {
+            obj1.status="success";
+            obj1.message="the difference of the given two numbers";
+            obj1.sum = result;
+        }
+       
+    }
+    
+    res.send(obj1);
+});
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
 module.exports = app;
